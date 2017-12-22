f49494f5bc4be15b11db5cdd4564a928309c07ea
maonicorporation <eparrilla70@gmail.com>
2017-12-22 16:11:58 +0100

    Delete bbdd_config.js

--- bbdd_config.js
+++
@@ -1,5 +0,0 @@
-exports.host = 'go.maoni.solutions'          //'go.maoni.solutions'  'localhost'
-exports.user = 'gomaonis_Aleix';
-exports.password = 'Aleix.2302';
-exports.database = 'gomaonis_maonibd';
-exports.emailpwd = 'PT8tn87_5WS7VD';
